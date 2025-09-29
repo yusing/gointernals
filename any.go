905b6bbede637aa831d9eee5be9c06e7fc9f42fd
yusing <yusing@6uo.me>
2025-09-29 15:03:42 +0800

    Add TypeFor and TypeOf generic functions to retrieve type information in any.go

--- any.go
+++ any.go
@@ -6,6 +6,15 @@ import (
 	"github.com/yusing/gointernals/abi"
 )
 
+func TypeFor[T any]() *abi.Type {
+	var v T
+	return EfaceOf(v).Type
+}
+
+func TypeOf[T any](v T) *abi.Type {
+	return EfaceOf(v).Type
+}
+
 func EfaceOf[T any](v T) *abi.Eface {
 	vAny := any(v)
 	return (*abi.Eface)(unsafe.Pointer(&vAny))
